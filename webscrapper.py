b94496eac90c4728592aadf0d89724b265ad5d69
Vibhor Agarwal <agarwalvibhor99@gmail.com>
2020-06-09 04:34:38 -0400

    Add files via upload

--- webscrapper.py
+++ webscrapper.py
@@ -1,10 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 import smtplib
-
+from datetime import datetime 
 URL = 'https://www.amazon.com/Apple-Watch-GPS-40mm-Aluminum/dp/B07XR5TRSZ/ref=sr_1_4?dchild=1&keywords=apple+watch&qid=1591688861&sr=8-4'
 
-headers = {#Put Your User Agent Here}
+headers = {#Use your user agent here }
 
 def price_check():
     page = requests.get(URL, headers= headers)
@@ -18,9 +18,15 @@ def price_check():
     price = float(price.split("$")[1])
     print(price)
 
-    if(price<250):
+    storePrice(title, price)
+
+    if(price>200):
         send_email(title, price)
 
+
+def storePrice(title, price):
+    priceHistory = open("Price", "a+")
+    priceHistory.write("Price of {} on {} is ${}".format(title, datetime.date(datetime.now())))
 
 def send_email(title, price):
     server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -28,7 +34,7 @@ def send_email(title, price):
     server.starttls()
     server.ehlo()
 
-    server.login('youremailid@domain', 'password')
+    server.login('youremailid@domain.com', 'password')
 
     subject = "Amazon Price Drop Alert"
     body = "Price for {} fell down! New price is $ {} \nCheck the link https://www.amazon.com/Apple-Watch-GPS-40mm-Aluminum/dp/B07XR5TRSZ/ref=sr_1_4?dchild=1&keywords=apple+watch&qid=1591688861&sr=8-4".format(title, price)
@@ -40,8 +46,11 @@ def send_email(title, price):
         'to email id',
         msg
     )
+
+
     print("Email successfully sent")
     server.quit()
 
-
-price_check()
+while(True):
+    price_check()
+    time.sleep(86400)
